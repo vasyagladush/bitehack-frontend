6f4bebab388784602394e29721a0b38738b16e27
Vasya Gladush <vasylyi2004@gmail.com>
2023-01-15 10:45:08 +0100

    complete calculator

---
+++ src/containers/Calculator/Constants.js
@@ -0,0 +1,29 @@
+export const UniversityConstants = [
+  {
+    maturaCalcCode: "AGH",
+    name: "AGH University of Science and Technology",
+  },
+];
+
+export const FieldOfStudyConstants = [
+  { name: "Ceramics", maturaCalcCode: "Ceramika" },
+  { name: "Social Informatics", maturaCalcCode: "Informatyka Społeczna" },
+  { name: "Sociology", maturaCalcCode: "Socjologia" },
+  { name: "Cultural Studies", maturaCalcCode: "Kulturoznawstwo" },
+];
+
+export const SubjectConstants = [
+  { name: "Physics", maturaCalcCode: "Fizyka" },
+  { name: "Biology", maturaCalcCode: "Biologia" },
+  { name: "Chemistry", maturaCalcCode: "Chemia" },
+  { name: "Informatics", maturaCalcCode: "Informatyka" },
+  { name: "Mathematics", maturaCalcCode: "Matematyka" },
+  { name: "Geography", maturaCalcCode: "Geografia" },
+  { name: "History", maturaCalcCode: "Historia" },
+  { name: "Philosophy", maturaCalcCode: "Filozofia" },
+];
+
+export const SubjectLevelConstants = [
+  { name: "Basic", maturaCalcCode: "Podstawowy" },
+  { name: "Advanced", maturaCalcCode: "Rozszerzony" },
+];
