5da63c13f16ff28623c65eaf029f35ee55e1d2d5
Patryk <patrykkurek@student.agh.edu.pl>
2023-01-15 11:52:37 +0100

    Debug ID dla konsultantów

--- src/containers/Chat/Consultants.js
+++ src/containers/Chat/Consultants.js
@@ -124,6 +124,7 @@ const Consultants = (props) => {
                 style={{ borderRadius: "100px" }}
                 src="https://i.kym-cdn.com/entries/icons/original/000/026/152/gigachad.jpg"
               />
+              <p>Debug ID: {consultant._id}</p>
               <h1>{consultant.fullname}</h1>
               {consultant.universities.map((university) => {
                 return (
